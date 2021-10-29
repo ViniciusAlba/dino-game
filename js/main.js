0e5672e8e7185b6861833a117825f1dd819c1f73
ViniciusAlba <vinicius.martins.vsm@outlook.com>
2021-10-29 16:14:51 -0300

    The script and the game is done. Now, I have to implement and update the page of game over.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 const ghost = document.querySelector('.ghost');
+const background = document.querySelector('.background');
 let isJumping = false;
+let position = 0;
+let score = document.querySelector('.score');
+let playerScore = 0;
+
+setInterval(() => {
+
+    playerScore += 10;
+
+    score.innerHTML = playerScore.toString();
+
+}, 1000);
 
 function handleKeyUp(event) {
 
@@ -17,15 +29,11 @@ function handleKeyUp(event) {
 
 function jump() {
 
-    let position = 0;
-
     isJumping = true;
 
     let upInterval = setInterval(() => {
 
-        if (position >= 150) {
-
-            // Down Ghost
+        if (position >= 200) {
 
             clearInterval(upInterval);
 
@@ -39,7 +47,6 @@ function jump() {
                 } else {
 
                     position -= 20;
-
                     ghost.style.bottom = position + 'px';
 
                 }
@@ -48,10 +55,7 @@ function jump() {
 
         } else {
 
-            // Up Ghost
-
             position += 20;
-        
             ghost.style.bottom = position + 'px';
 
         }
@@ -59,5 +63,42 @@ function jump() {
     }, 20);
 
 }
+
+function createPumpkins() {
+
+    const pumpkins = document.createElement('div');
+    let pumpkinsPosition = 1800;
+    let randomTime = Math.random() * 6000 + Math.random() * 6000;
+
+    pumpkins.classList.add('pumpkins');
+    pumpkins.style.left = pumpkinsPosition + 'px';
+    background.appendChild(pumpkins);
+
+    let leftInterval = setInterval(() => {
+
+        if (pumpkinsPosition < -80) {
+
+            clearInterval(leftInterval);
+            background.removeChild(pumpkins);
+
+        } else if (pumpkinsPosition > 0 && pumpkinsPosition < 80 && position < 80) {
+
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+
+        } else {
+
+            pumpkinsPosition -= 10;
+            pumpkins.style.left = pumpkinsPosition + 'px';
+
+        }
+
+    }, 20);
+    
+    setTimeout(createPumpkins, randomTime);
+
+}
+
+createPumpkins();
 
 document.addEventListener('keyup', handleKeyUp);
